daca8e8d2c5efa6598124cccd1e6abcb3d9c37e4
Zed Damasky <a.yambulatov@yandex.ru>
2026-06-11 20:22:35 +0400

    fix(sections): change AboutLawyers section

--- src/sections/AboutLawyers/AboutLawyers.tsx
+++ src/sections/AboutLawyers/AboutLawyers.tsx
@@ -9,11 +9,9 @@ export function AboutLawyers() {
     <section
       id="about"
       ref={sectionRef}
-      //className="pt-20 md:pt-24 pb-16 md:pb-20 bg-light animate-on-scroll min-h-screen"
       className="section-padding bg-light animate-on-scroll scroll-mt-25 min-h-screen"
     >
       <div className="container-custom px-4">
-        {/*<div className="text-center mb-16 md:mb-24 mt-16">*/}
         <div className="text-center md:mb-10 mt-25">
           <h2 className="text-3xl md:text-4xl font-bold text-dark">
             Практикующие адвокаты
