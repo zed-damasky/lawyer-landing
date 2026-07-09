1ecfb6825eabcdfe7332d27a0365053b3d8d2d64
Zed Damasky <a.yambulatov@yandex.ru>
2026-07-09 19:47:33 +0400

    chore: initial release

---
+++ src/data/lawyers.ts
@@ -0,0 +1,40 @@
+import type { Lawyer } from "@/types/lawyer";
+/*
+  todo: парсер и отдельные json на каждого?
+*/
+
+export const lawyers: Lawyer[] = [
+  {
+    id: "ponkin",
+    name: "Понкин  Сергей Анатольевич",
+    photo: "/images/lawyers/ponkin.png",
+    imageProfile: "/images/lawyers/profileImage/male_opt.svg",
+    workplace:
+      "Адвокат Башкирской республиканской коллегии адвокатов (Ишимбайский городской филиал)",
+    regNumber: "03/2248",
+    practices: [
+      "Гражданские и уголовные дела",
+      "Трудовое и пенсионное право",
+      "Арбитражные и корпоративные споры",
+      "Административное право, дела о ДТП",
+      "Банкротство граждан и организаций",
+    ],
+  },
+  {
+    id: "iambulatova",
+    name: "Ямбулатова  Ниля Марсовна",
+    photo: "/images/lawyers/iambulatova.png",
+    imageProfile: "/images/lawyers/profileImage/female_opt.svg",
+    workplace:
+      "Адвокат Башкирской республиканской коллегии адвокатов (Ишимбайский городской филиал)",
+    regNumber: "03/1555",
+    practices: [
+      "Гражданские и уголовные дела",
+      "Семейные споры",
+      "Жилищное право",
+      "Земельное право",
+      "Наследственные споры",
+      "Споры с государственными органами"
+    ],
+  },
+];
